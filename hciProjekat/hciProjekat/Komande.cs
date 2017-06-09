d0d460f466749f7b75daa6d1523c578ac00ad4b6
Nikolina <rnikolina1202@gmail.com>
2017-06-09 18:34:31 +0200

    dodavanje, brisanje

---
+++ hciProjekat/hciProjekat/Komande.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace hciProjekat
+{
+    public static class Komande
+    {
+        public static readonly RoutedUICommand Izmijeni = new RoutedUICommand(
+          "Izmijeni",
+          "Izmijeni",
+          typeof(Komande),
+          new InputGestureCollection()
+          {
+                new KeyGesture(Key.U, ModifierKeys.Control),
+            }
+          );
+    }
+}
---
+++ hciProjekat/hciProjekat/Komande/Komande.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace hciProjekat.Komande
+{
+    public static class Komande
+    {
+        public static readonly RoutedUICommand Izmijeni = new RoutedUICommand(
+          "Izmijeni",
+          "Izmijeni",
+          typeof(Komande),
+          new InputGestureCollection()
+          {
+                new KeyGesture(Key.U, ModifierKeys.Control),
+            }
+          );
+
+        public static readonly RoutedUICommand Novi = new RoutedUICommand(
+          "Novi",
+          "Novi",
+          typeof(Komande),
+          new InputGestureCollection()
+          {
+                new KeyGesture(Key.N, ModifierKeys.Control),
+            }
+          );
+    }
+}
