b363e688077399755ade4d62c762d70a87d45d5e
Nikolina <rnikolina1202@gmail.com>
2017-06-13 08:03:17 +0200

    help

---
+++ hciProjekat/hciProjekat/JavaScriptControlHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hciProjekat
+{
+   
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        [ComVisible(true)]
+        class JavaScriptControlHelper
+        {
+            MainWindow prozor;
+            PredmetiPage dodajPredmet;
+            public JavaScriptControlHelper(MainWindow w)
+            {
+                prozor = w;
+            }
+
+            public JavaScriptControlHelper(PredmetiPage w)
+            {
+                dodajPredmet = w;
+            }
+
+        public void RunFromJavascript(string param)
+            {
+                prozor.doThings(param);
+            }
+        }
+    }
+
--- hciProjekat/hciProjekat/MainWindow.xaml.cs
+++ hciProjekat/hciProjekat/MainWindow.xaml.cs
@@ -203,5 +203,9 @@ namespace hciProjekat
                 page.IzmijeniUcionicu();
 
         }
+        public void doThings(string param)
+        {
+            Title = param;
+        }
     }
 }
